f4ced65a15e857f5be5ceca308e91ace21d4d391
ikir85 <riki.tinoza@hotmail.com>
2018-07-13 14:44:47 +0200

    modif_index

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ require_once __DIR__ .'/Include/init.php';
 
 require __DIR__ .'/layout/top.php';
 ?>
-       <h1>Acceuil</h1>
+       <h1>Page d'Acceuil</h1>
 <?php
 require __DIR__ .'/layout/bottom.php';
 ?>
